de1bdcf9535ca2420ff234ae1263734846638117
Aleksandar Georgiev <aleksandar.kystendil.georgiev@gmail.com>
2021-08-06 20:20:21 +0300

    day four build

--- components/Banner.js
+++ components/Banner.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import hedPic from "../public/header.jpg";
 
 function Banner() {
   return (
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-      <Image
-        src="https://links.papareact.com/0fm"
-        layout="fill"
-        objectFit="cover"
-      />
-      <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+      <Image src={hedPic} layout="fill" objectFit="cover" />
+      <div className="absolute top-1/4 w-full text-left ml-20 ">
+        <p className="text-lg sm:text-7xl text-white">Olympian & </p>
+        <p className="text-lg sm:text-7xl  text-white">Paralumpian </p>
+        <p className="text-lg sm:text-7xl  text-white">Online </p>
+        <p className="text-lg sm:text-7xl  text-white"> Expiriences </p>
         <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
           I'm flexible
         </button>
--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiZ2VvcmdpZXZhbGVrc2FuZGFyIiwiYSI6ImNrczBpMmwwcjEybW4zMm85dGxuZmZrOWkifQ.l2Kr2U9K6MV84PSqmz-rEA",
+  },
 };
