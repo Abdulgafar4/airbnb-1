6b5c39d339a8d0ae795f239fcb7d0f74dd4708e2
AleksRG <aleksandar.kystendil.georgiev@gmai.com>
2021-08-08 14:55:37 +0300

    login icon change

--- components/Header.js
+++ components/Header.js
@@ -73,14 +73,14 @@ function Header({ placeholder }) {
         <SearchIcon className="hidden md:inline-flex h-8 bg-yellow-500  text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
       {/*right*/}
-      <div className="flex items-center space-x-4 justify-end text-gray-700">
+      <div className="flex items-center space-x-4 justify-end text-white">
         <p className="hidden md:inline cursor-pointer hover:underline ">
           Become a host
         </p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
-        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-          <MenuIcon className="h-6 cursor-pointer" />
-          <UserCircleIcon className="h-6 cursor-pointer animate-bounce" />
+        <div className="flex items-center space-x-2 border-1 p-2 rounded-full text-gray-700 bg-white">
+          <MenuIcon className="h-6 cursor-pointer " />
+          <UserCircleIcon className="h-6 cursor-pointer animate-bounce " />
         </div>
       </div>
       {searchInput && (
