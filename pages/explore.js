c906dc732253e9842c9ec29c125279f08a292cf8
AleksRG <aleksandar.kystendil.georgiev@gmai.com>
2021-08-08 16:55:44 +0300

    explore page update

--- pages/explore.js
+++ pages/explore.js
@@ -118,6 +118,31 @@ function explore() {
         <section className="relative py-2 cursor-pointer hover:scale-105 transform transition duration-300 ease-out ">
           <div className="relative h-96 min-w-[300px]">
             <Image
+              src={four}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-2xl"
+            />
+          </div>
+          <div className="absolute top-32 left-12 h-auto p-4 bg-white/20 backdrop-blur-md rounded">
+            <h3 className="text-4xl mb-3 w-64 text-white hover:text-pink-600">
+              Inside the Games
+            </h3>
+            <p>
+              Experience the Tokyo 2020 Games along with the insiders, right
+              from home.
+            </p>
+            <button
+              className="text-white bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 px-8 py-3 shadow-md rounded-full font-bold my-4  hover:shadow-xl active:scale-90 transition duration-150 hover:underline "
+              onClick={() => router.push("/moreInfo")}
+            >
+              Let`s go!
+            </button>
+          </div>
+        </section>
+        <section className="relative py-2 cursor-pointer hover:scale-105 transform transition duration-300 ease-out ">
+          <div className="relative h-96 min-w-[300px]">
+            <Image
               src={five}
               layout="fill"
               objectFit="cover"
